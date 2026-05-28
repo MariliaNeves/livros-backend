6721c4f5405603b0547025a32eb6a09ddbc1f67e
Marilia Neves <marilia.neves@mj.gov.br>
2026-05-28 08:16:12 -0300

    restante das operacoes de  assunto

--- src/main/java/br/com/livros/controller/AssuntoController.java
+++ src/main/java/br/com/livros/controller/AssuntoController.java
@@ -1,11 +1,12 @@
 package br.com.livros.controller;
 
+import br.com.livros.dto.AssuntoRequest;
 import br.com.livros.dto.AssuntoResponse;
 import br.com.livros.entity.Assunto;
 import br.com.livros.service.AssuntoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +23,28 @@ public class AssuntoController {
     @GetMapping
     public List<AssuntoResponse> listar(){
         return assuntoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public AssuntoResponse buscarPorId(@PathVariable Integer id) {
+        return assuntoService.buscarPorId(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AssuntoResponse criar(@Valid @RequestBody AssuntoRequest request) {
+        return assuntoService.criar(request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Integer id) {
+        assuntoService.excluir(id);
+    }
+
+
+    @PutMapping("/{id}")
+    public AssuntoResponse atualizar(@PathVariable Integer id, @Valid @RequestBody AssuntoRequest request) {
+        return assuntoService.atualizar(id, request);
     }
 }
--- src/main/java/br/com/livros/service/AssuntoService.java
+++ src/main/java/br/com/livros/service/AssuntoService.java
@@ -1,7 +1,9 @@
 package br.com.livros.service;
 
+import br.com.livros.dto.AssuntoRequest;
 import br.com.livros.dto.AssuntoResponse;
 import br.com.livros.entity.Assunto;
+import br.com.livros.exception.ResourceNotFoundException;
 import br.com.livros.mapper.AssuntoMapper;
 import br.com.livros.repository.AssuntoRepository;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,35 @@ public class AssuntoService {
     @Transactional(readOnly = true)
     public List<AssuntoResponse> listar(){
         return assuntoRepository.findAll().stream().map(assuntoMapper::toResponse).toList();
+    }
+
+    @Transactional
+    public AssuntoResponse criar(AssuntoRequest request){
+        Assunto assunto = assuntoMapper.toEntity(request);
+        return  assuntoMapper.toResponse(assuntoRepository.save(assunto));
+    }
+
+    @Transactional(readOnly = true)
+    public AssuntoResponse buscarPorId(Integer id) {
+        return assuntoMapper.toResponse(buscarEntidade(id));
+    }
+
+    @Transactional
+    public AssuntoResponse atualizar(Integer id, AssuntoRequest request) {
+        Assunto assunto = buscarEntidade(id);
+        assuntoMapper.updateEntity(assunto, request);
+        return assuntoMapper.toResponse(assuntoRepository.save(assunto));
+    }
+
+    @Transactional
+    public void excluir(Integer id) {
+        Assunto assunto = buscarEntidade(id);
+        assuntoRepository.delete(assunto);
+    }
+
+    private Assunto buscarEntidade(Integer id) {
+        return assuntoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Assunto nao encontrado: " + id));
     }
 
 }
